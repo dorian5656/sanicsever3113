ae9ca7cb949558e63909cd8638913117a7656267
wuhao <wuhooooo5@gmail.com>
2023-04-25 15:15:23 +0800

    优化yml编码问题

--- FileTools/YmlTools.py
+++ FileTools/YmlTools.py
@@ -2,13 +2,13 @@
 # -*-coding:utf-8-*-
 import yaml
 
-
-class Yml_Curd(object):
+# yml增删改查
+class YmlCurd(object):
     """
     yml操作
     """
 
-    def __init__(self, yml_path):
+    def __init__(self, yml_path=""):
         self.yml_path = yml_path
 
     def read_yaml(self):
@@ -16,12 +16,13 @@ class Yml_Curd(object):
         读取yml文件
         :return: 全部数据,为字典
         """
-        with open(self.yml_path, 'r') as f:
+        with open(self.yml_path, 'r',encoding='utf-8') as f:
             result = yaml.safe_load(f)
         # print(result)
         return result
 
 
 # if __name__ == '__main__':
-#     YC = Yml_Curd(R"demo_20230418.yml")
+#     YC = YmlCurd(R"demo_20230418.yml")
+#     YC = YmlCurd(R"../ConfigTools/DatabaseConfig/PgServe.yml")
 #     YC.read_yaml()
